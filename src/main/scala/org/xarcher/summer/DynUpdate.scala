a3a3368b5694a56fc695d470563738937d5a3c55
djx314 <djx314@sina.cn>
2015-08-23 15:05:42 +0800

    buautiful code.

--- src/main/scala/org/xarcher/summer/DynUpdate.scala
+++ src/main/scala/org/xarcher/summer/DynUpdate.scala
@@ -1,5 +1,7 @@
 package org.xarcher.summer
 
+import slick.driver.JdbcProfile
+
 import scala.language.existentials
 import slick.lifted._
 import slick.dbio._
@@ -15,10 +17,25 @@ case class DynData[E <: AbstractTable[_], U](colTra: E => Rep[U], value: U)(impl
 
 trait DynUpdate {
 
+  private def changHead[E <: AbstractTable[_]](change: DynData[E, _]): DynamicUpdateChange[E] = change match {
+    case change@DynData(currentColTran, currentValue) =>
+      import change._
+      val colunm: E => Tuple1[Rep[DataType]] = (table: E) => Tuple1(currentColTran(table))
+      val value =  Tuple1(currentValue)
+      implicit val dynTuple1Shape = new TupleShape[FlatShapeLevel, Tuple1[Rep[DataType]], Tuple1[DataType], Tuple1[Rep[DataType]]](dynShape)
+      new DynamicUpdateChange[E] {
+        type ColType = Tuple1[Rep[DataType]]
+        type ValType = Tuple1[DataType]
+        val col = colunm
+        val data = value
+        val shape = dynTuple1Shape
+      }
+  }
+
   def update[E <: AbstractTable[_]](dataList: List[DynData[E, _]]): Option[DynamicUpdateChange[E]] = {
     dataList match {
       case change :: tail =>
-        Option(tail.foldLeft(Change(change)) { (r, c) =>
+        Option(tail.foldLeft(changHead(change)) { (r, c) =>
           r.append(c)
         })
       case Nil => None
@@ -51,23 +68,6 @@ trait DynamicUpdateChange[E <: AbstractTable[_]] {
       }
   }
 
-}
-
-private object Change {
-  def apply[E <: AbstractTable[_]](change: DynData[E, _]): DynamicUpdateChange[E] = change match {
-    case change@DynData(currentColTran, currentValue) =>
-      import change._
-      val colunm: E => Tuple1[Rep[DataType]] = (table: E) => Tuple1(currentColTran(table))
-      val value =  Tuple1(currentValue)
-      implicit val dynTuple1Shape = new TupleShape[FlatShapeLevel, Tuple1[Rep[DataType]], Tuple1[DataType], Tuple1[Rep[DataType]]](dynShape)
-      new DynamicUpdateChange[E] {
-        type ColType = Tuple1[Rep[DataType]]
-        type ValType = Tuple1[DataType]
-        val col = colunm
-        val data = value
-        val shape = dynTuple1Shape
-      }
-  }
 }
 
 object DynUpdate extends DynUpdate
