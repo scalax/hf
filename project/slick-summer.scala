602daa46d2d2efe43ab099f63461b1691b18a95f
djx314 <djx314@sina.cn>
2015-08-23 19:35:27 +0800

    new API.

--- project/slick-summer.scala
+++ project/slick-summer.scala
@@ -19,7 +19,7 @@ object `slick-summer` extends Build {
   .settings(
     name := "slick-summer",
     libraryDependencies ++= Seq(
-      "com.typesafe.slick" %% "slick" % "3.1.0-M2",
+      "com.typesafe.slick" %% "slick" % "3.0.2",
       "org.scalatest" %% "scalatest" % "2.2.4" % "test",
       "com.h2database" % "h2" % "1.4.187" % "test",
       "org.slf4j" % "slf4j-simple" % "1.7.12" % "test",
